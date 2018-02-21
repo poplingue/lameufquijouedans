b2af5425c284980819d40bb1468298fdcf4b2706
Pauline Gaudet-Chardonnet <pauline.gaudet-chardonnet@lip6.fr>
2018-02-21 20:04:45 +0100

    animation onFocus/onBlur

--- js/app.js
+++ js/app.js
@@ -1,162 +1,186 @@
 import React from 'react';
-import { render } from 'react-dom';
-import Actor from './Actor';
 import Movie from './Movie';
 
 class App extends React.Component {
 
-  constructor(props) {
-    super(props)
-    this.apiKey = '?api_key=148d9341acd58f310f70e4660a4a9add'
-    this.apiUrl = 'https://api.themoviedb.org/3/'
-    this.array = []
-    this.arrayActors = []
-    this.obj = {}
+    constructor(props) {
+        super(props)
+        this.apiKey = '?api_key=148d9341acd58f310f70e4660a4a9add'
+        this.apiUrl = 'https://api.themoviedb.org/3/'
+        this.array = []
+        this.arrayActors = []
+        this.obj = {}
 
-    this.state = {
-      movieList: [],
-      actorList: [],
-      search: "",
-      currentMovie: null
+        this.state = {
+            movieList: [],
+            actorList: [],
+            search: "",
+            currentMovie: null,
+            ready: false,
+            value: ""
+        }
+
+        this.searchMovies = this.searchMovies.bind(this)
     }
 
-    this.searchMovies = this.searchMovies.bind(this)
-  }
-  searchMovies(e) {
-    e.preventDefault()
+    searchMovies(e) {
+        e.preventDefault()
 
-    let search = (e.type === 'submit') ? this.state.search : e.target.value
-    this.setState({ search: search })
+        let search = (e.type === 'submit') ? this.state.search : e.target.value
+        this.setState({search: search, value: e.target.value})
 
-    this.query = this.transformValueToQuery(search)
+        this.query = this.transformValueToQuery(search)
 
-    this.getMovies(this.apiUrl + 'search/movie' + this.apiKey + '&query=' + this.query)
-      .then((movies) => {
-        this.setState({ movieList: movies })
-      }).catch((err) => {
-        console.log(err)
-      })
-  }
-  transformValueToQuery(value) {
-    let c = ''
-    let a = []
-    for (let i = 0; i < value.length; i++) {
-      c = (value[i] === ' ') ? '%20' : value[i]
-      a.push(c)
+        this.getMovies(this.apiUrl + 'search/movie' + this.apiKey + '&query=' + this.query)
+            .then((movies) => {
+                this.setState({movieList: movies})
+            }).catch((err) => {
+            console.log(err)
+        })
     }
-    return a.join('')
-  }
-  searchCast(movieId, id) {
 
-    this.getCast(movieId).then((casting) => {
-      return this.updateArrayActors(casting)
-    }).then((actorList) => {
-      this.setState({ currentMovie: id, actorList: actorList })
-    })
-  }
-  updateArrayActors(casting) {
-
-    return new Promise((resolve, reject) => {
-      let arrayActors = []
-      let obj = {}
-
-      for (let i = 0; i < casting.length; i++) {
-        obj = {
-          "name": casting[i].name,
-          "img": casting[i].profile_path
+    transformValueToQuery(value) {
+        let c = ''
+        let a = []
+        for (let i = 0; i < value.length; i++) {
+            c = (value[i] === ' ') ? '%20' : value[i]
+            a.push(c)
         }
-        arrayActors.push(obj)
+        return a.join('')
+    }
 
-        if (casting.length === arrayActors.length) {
-          resolve(arrayActors)
-          return
-        }
-      }
-    })
-  }
-  getCast(movieId) {
+    searchCast(movieId, id) {
 
-    let self = this
-    return new Promise((resolve, reject) => {
+        this.getCast(movieId).then((casting) => {
+            return this.updateArrayActors(casting)
+        }).then((actorList) => {
+            this.setState({currentMovie: id, actorList: actorList})
+        })
+    }
 
-      if (!movieId) {
-        reject(new Error("no movie id"))
-      }
+    updateArrayActors(casting) {
 
-      let url = ""
-      let req = new XMLHttpRequest()
-      self.array = []
+        return new Promise((resolve, reject) => {
+            let arrayActors = []
+            let obj = {}
 
-      url = self.apiUrl + 'movie/' + movieId + '/credits' + self.apiKey
-      req.open('GET', url, true)
+            for (let i = 0; i < casting.length; i++) {
+                obj = {
+                    "name": casting[i].name,
+                    "img": casting[i].profile_path
+                }
+                arrayActors.push(obj)
 
-      req.onreadystatechange = function () {
-
-        if (this.readyState === 4) {
-
-          if (this.status === 200 && (JSON.parse(this.response).cast).length > 0) {
-
-            for (let x = 0; x < (JSON.parse(this.response).cast).length; x++) {
-              self.array.push(JSON.parse(this.response).cast[x])
-
-              // need to wait the end of loop to resolve the promise
-              setTimeout(() => {
-                resolve(self.array)
-              }, 0)
-
+                if (casting.length === arrayActors.length) {
+                    resolve(arrayActors)
+                    return
+                }
             }
-          }
-        }
-      }
-      req.send()
-    })
-  }
-  getMovies(url) {
-    var self = this
-    return new Promise((resolve, reject) => {
+        })
+    }
 
-      if (this.query) {
-        //request from value movie
-        var httpRequest = new XMLHttpRequest()
-        httpRequest.open('GET', url)
+    getCast(movieId) {
 
-        httpRequest.onreadystatechange = function () {
-          if (this.readyState === 4) {
+        let self = this
+        return new Promise((resolve, reject) => {
 
-            if (this.status === 200) {
-              resolve(JSON.parse(this.responseText).results)
+            if (!movieId) {
+                reject(new Error("no movie id"))
+            }
+
+            let url = ""
+            let req = new XMLHttpRequest()
+            self.array = []
+
+            url = self.apiUrl + 'movie/' + movieId + '/credits' + self.apiKey
+            req.open('GET', url, true)
+
+            req.onreadystatechange = function () {
+
+                if (this.readyState === 4) {
+
+                    if (this.status === 200 && (JSON.parse(this.response).cast).length > 0) {
+
+                        for (let x = 0; x < (JSON.parse(this.response).cast).length; x++) {
+                            self.array.push(JSON.parse(this.response).cast[x])
+
+                            // need to wait the end of loop to resolve the promise
+                            setTimeout(() => {
+                                resolve(self.array)
+                            }, 0)
+
+                        }
+                    }
+                }
+            }
+            req.send()
+        })
+    }
+
+    getMovies(url) {
+        var self = this
+        return new Promise((resolve, reject) => {
+
+            if (this.query) {
+                //request from value movie
+                var httpRequest = new XMLHttpRequest()
+                httpRequest.open('GET', url)
+
+                httpRequest.onreadystatechange = function () {
+                    if (this.readyState === 4) {
+
+                        if (this.status === 200) {
+                            resolve(JSON.parse(this.responseText).results)
+                        } else {
+                            reject(new Error("status response : " + this.status))
+                        }
+                    }
+                }
+                httpRequest.send()
             } else {
-              reject(new Error("status response : " + this.status))
+                reject('write something')
             }
-          }
-        }
-        httpRequest.send()
-      } else {
-        reject('write something')
-      }
-    })
-  }
+        })
+    }
 
-  render() {
-    return (
-      <div id="wrap-form">
-        <form onSubmit={this.searchMovies}>
-          <input type="text" autoComplete="off" placeholder='Billy Elliot, Titanic...' id="value" value={this.state.value} onChange={this.searchMovies} />
-          <input type="submit" value="" id="send" />
-        </form>
-        <ul className='movie-list'>
-          {this.state.movieList.map((movie, id) => {
-            return <li onClick={this.searchCast.bind(this, movie.id, id)} key={id}>
-              <Movie
-                id={id}
-                currentMovie={this.state.currentMovie}
-                poster={movie.poster_path}
-                movie={movie.original_title}
-                actorList={this.state.actorList} />
-            </li>;
-          })}
-        </ul>
-      </div >)
-  }
+    appReady(e) {
+        if (e.type == 'blur' && this.state.value == '') {
+            document.getElementById('wrap-container').className = ''
+        } else {
+            document.getElementById('wrap-container').className = 'ready'
+        }
+    }
+
+    render() {
+
+        return (
+            <div id="wrap-form">
+              <form onSubmit={this.searchMovies}>
+                <input
+                    type="text"
+                    autoComplete="off"
+                    placeholder='Billy Elliot, Titanic...'
+                    id="value"
+                    value={this.state.value}
+                    onChange={(e)=>this.searchMovies(e)}
+                    onFocus={(e) => this.appReady(e)}
+                    onBlur={(e) => this.appReady(e)}/>
+                <input type="submit" value="" id="send"/>
+              </form>
+              <ul className='movie-list'>
+                  {this.state.movieList.map((movie, id) => {
+                      return <li onClick={this.searchCast.bind(this, movie.id, id)} key={id}>
+                        <Movie
+                            id={id}
+                            currentMovie={this.state.currentMovie}
+                            poster={movie.poster_path}
+                            movie={movie.original_title}
+                            actorList={this.state.actorList}/>
+                      </li>;
+                  })}
+              </ul>
+            </div>)
+    }
 }
+
 export default App;
--- lameufquijouedans/celery/celeryconfig.py
+++ lameufquijouedans/celery/celeryconfig.py
@@ -1,8 +1,9 @@
 BROKER_URL = 'amqp://guest:guest@127.0.0.1:5672//'
-CELERY_RESULT_BACKEND = 'rpc://'
-# BACKEND = 'db+mysql://tasks:root@localhost:3306/tasks'
+CELERY_BACKEND = 'sqlite:///Users/pgc/Documents/database_files/filestorage.db'
+
+# CELERY_RESULT_BACKEND = 'db+mysql://poplingue:VBWfHlksa24T9k9n@127.0.0.1:3306/tasks'
 # CELERY_ROUTES = {'queue0': {'queue': 'queue_test0'}}
 # CELERY_TASK_SERIALIZER = 'json'
-# CELERY_RESULT_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
 # CELERY_ACCEPT_CONTENT = ['json']
 
